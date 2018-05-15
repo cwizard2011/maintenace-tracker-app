b16cd5f95c268fdc364ee8efca3cdcaa0c8266a1
Peter Adeoye <cwizard2011@gmail.com>
2018-05-15 15:01:41 +0100

    [Feature #157592911] Add login Modal

--- UI/js/modal.js
+++ UI/js/modal.js
@@ -1,26 +1,53 @@
 const signUpForm = document.getElementById('signUpForm');
+const loginForm = document.getElementById('loginForm');
+const loginModal = document.getElementById('loginModal');
+const logBtn = document.getElementById('login')
 const modal = document.getElementById('signupModal');
-const btn = document.getElementById('sign-up');
+const signBtn = document.getElementById('sign-up');
 const signUp = document.getElementById('signUp')
+const logIn = document.getElementById('logIn')
 const span = document.getElementsByClassName("close")[0];
+const loginSpan = document.getElementsByClassName("login-close")[0];
 
-btn.onclick = function() {
+signBtn.onclick = () => {
     modal.style.display = "block";
 }
 signUp.onclick = () => {
   modal.style.display = "block"
 }
 
-span.onclick = function() {
+span.onclick = () => {
     modal.style.display = "none";
 }
 
-window.onclick = function(event) {
+window.onclick = (event) => {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
 signUpForm.onsubmit = (e) => {
+  e.preventDefault();
+  window.location.href = './index.html';
+
+};
+
+// Login button
+
+logBtn.onclick = () => {
+    loginModal.style.display = "block";
+}
+logIn.onclick = () => {
+    loginModal.style.display = "block"
+}
+loginSpan.onclick = () => {
+    loginModal.style.display = "none";
+}
+window.onclick = (event) => {
+    if (event.target == loginModal) {
+        loginModal.style.display = "none";
+    }
+}
+loginForm.onsubmit = (e) => {
   e.preventDefault();
   window.location.href = './index.html';
 
