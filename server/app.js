fb7dc98e603dc41d3bafb9b5c0b77f430e37e185
Peter Adeoye <cwizard2011@gmail.com>
2018-05-17 11:53:14 +0100

    [Chore #157647107] Setup eslint style guide

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const port = process.env.PORT || PORT
+const port = process.env.PORT;
 
 const app = express();
-app.get('/', (req, res) => res.send('Welcome to maintenance app'))
+app.get('/', (req, res) => res.send('Welcome to maintenance app'));
 
-app.listen(port, () => console.log('Example app listening on port 3000!'))
+app.listen(port, () => console.log('Example app listening on port 3000!'));
