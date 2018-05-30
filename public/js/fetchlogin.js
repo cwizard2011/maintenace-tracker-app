c9c0a8bb2ed0742c9126bfd2edb35826f781decf
Peter Adeoye <cwizard2011@gmail.com>
2018-05-30 12:52:15 +0100

    ft(fetch-user-login): post user login to api
    - refactor index.html to allow login
    - create fetch login file
    - refactor fetch signup file
    - refactor login and signup modal
    - refactor admindashboard and dashboard
    [finishes #157917339]

---
+++ public/js/fetchlogin.js
@@ -0,0 +1,40 @@
+const loginurl = 'https://peter-maintenance-app.herokuapp.com/api/v1';
+
+/*  global document:true, fetch:true, window:true */
+/*  eslint no-undef: "error"  */
+document.getElementById('login-user').value = '';
+document.getElementById('login-password').value = '';
+
+const loginForm = document.getElementById('loginForm');
+loginForm.onsubmit = (e) => {
+  e.preventDefault();
+
+  const loginUser = document.getElementById('login-user').value;
+  const loginPassword = document.getElementById('login-password').value;
+  const loginError = document.getElementById('login-error');
+  const newLogin = {
+    username: loginUser,
+    password: loginPassword,
+  };
+  fetch(`${loginurl}/auth/login`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newLogin),
+  })
+    .then(res => res.json())
+    .then((users) => {
+      if (users.status === 'fail') {
+        loginError.innerHTML = users.message;
+      }
+      if (users.data.role === 'users') {
+        window.localStorage.setItem('token', users.data.token);
+        window.location.href = 'pages/dashboard.html';
+      } else if (users.data.role === 'admin') {
+        window.localStorage.setItem('token', users.data.token);
+        window.location.href = 'pages/admindashboard.html';
+      }
+    });
+};
