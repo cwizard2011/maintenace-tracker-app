d0a93c2b41f43cb82c15d59df6f29ce25798c9ba
Peter Adeoye <cwizard2011@gmail.com>
2018-05-30 21:07:18 +0100

    ft(fetch-user-request):fetch all requests for user
    - create a css collapsible to toggle request details
    - create a fetch request js file to fetch data from api
    - refactor dashboard.html to render request from api
    [finishes #157917400]

--- public/js/expand.js
+++ public/js/expand.js
@@ -1,15 +1,19 @@
-const coll = document.getElementsByClassName("btn");
-let i;
+/*  global  document:true, */
+/*  eslint no-undef: "error"  */
+const expandBtn = () => {
+  const coll = document.getElementsByClassName('btn');
+  let i;
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const expand = this.nextElementSibling;
-    if (expand.style.maxHeight){
-      expand.style.maxHeight = null;
-    } else {
-      expand.style.maxHeight = expand.scrollHeight + "px";
-    
-    } 
-  });
-}
+  for (i = 0; i < coll.length; i += 1) {
+    coll[i].addEventListener('click', () => {
+      this.classList.toggle('active');
+      const expand = this.nextElementSibling;
+      if (expand.style.maxHeight) {
+        expand.style.maxHeight = null;
+      } else {
+        expand.style.maxHeight = `${expand.scrollHeight}px`;
+      }
+    });
+  }
+};
+expandBtn();
