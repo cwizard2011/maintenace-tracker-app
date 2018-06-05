efd47e751fd8d8a1359cd0c1995be9cd332e835b
Peter Adeoye <cwizard2011@gmail.com>
2018-06-05 17:23:13 +0100

    ft(approve-user-request):approve a pending request
    - set up approve request function
    - refactor form.css modal class to resolve modal conflict
    [finishes #157917969]

--- public/js/request.js
+++ public/js/request.js
@@ -1,23 +1,25 @@
+/*  global window:true, document:true */
+/*  eslint no-undef: "error"  */
 const modal = document.getElementById('reqModal');
-const btn = document.getElementById("reqBtn");
-const span = document.getElementsByClassName("close")[0];
+const btn = document.getElementById('reqBtn');
+const span = document.getElementsByClassName('close')[0];
 const reqForm = document.getElementById('reqForm');
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+btn.onclick = () => {
+  modal.style.display = 'block';
+};
 
-span.onclick = function() {
-    modal.style.display = "none";
-}
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+span.onclick = () => {
+  modal.style.display = 'none';
+};
+
+window.onclick = (event) => {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
 reqForm.onsubmit = (e) => {
   e.preventDefault();
   window.location.href = './dashboard.html';
-
 };
