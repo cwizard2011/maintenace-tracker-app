0c0d660bf0589a30e16dd03642223d9d2717ee39
Peter Adeoye <cwizard2011@gmail.com>
2018-06-12 10:39:30 +0100

    ft(get-profile-image):retrieve user profile image
    - method for retrieving profile image
    - write test for image upload and get profile image
    [finishes #158235547]

---
+++ server/tests/upload.test.js
@@ -0,0 +1,64 @@
+import chai from 'chai';
+import path from 'path';
+import request from 'supertest';
+import app from '../app';
+
+/*  global describe:true, it:true, before:true */
+/*  eslint no-undef: "error"  */
+const { expect } = chai;
+let userToken;
+
+describe('Request controller', () => {
+  before(async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/login')
+      .set('Accept', 'application/json')
+      .send({
+        username: 'juliet',
+        password: 'password123',
+      });
+    userToken = res.body.data.token;
+  });
+
+  describe('Image Controller', () => {
+    it('should not get image for non-authenticated user', async () => {
+      const res = await request(app)
+        .get('/api/v1/users/profile/image')
+        .set('Accept', 'application/json')
+        .expect(401);
+      expect(res.body).to.have.a.property('message');
+      expect(res.body.message).to.equal('Please login with your username and password');
+      expect(res.body.status).to.equal('fail');
+    });
+    it('should return 200 if user doesnt have image', async () => {
+      const res = await request(app)
+        .get('/api/v1/users/profile/image')
+        .set('Accept', 'application/json')
+        .set('token', userToken)
+        .expect(200);
+      expect(res.body).to.have.a.property('message');
+      expect(res.body.message).to.equal('Please upload a profile image');
+      expect(res.body.status).to.equal('success');
+    });
+    it('should not post image for non-authenticated user', async () => {
+      const res = await request(app)
+        .put('/api/v1/users/profile/image')
+        .set('Accept', 'application/json')
+        .attach('image', path.join(__dirname, 'images/profile.jpeg'))
+        .expect(401);
+      expect(res.body).to.have.a.property('message');
+      expect(res.body.message).to.equal('Please login with your username and password');
+      expect(res.body.status).to.equal('fail');
+    });
+    it('should return error 400 if no image is attached', async () => {
+      const res = await request(app)
+        .put('/api/v1/users/profile/image')
+        .set('Accept', 'application/json')
+        .set('token', userToken)
+        .expect(400);
+      expect(res.body).to.have.a.property('message');
+      expect(res.body.message).to.equal('No image was attached, please attach an image');
+      expect(res.body.status).to.equal('fail');
+    });
+  });
+});
